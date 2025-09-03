eb153f51ffda2649db8c235ca726acc443cedd5d
robson <robsonraphael2001@gmail.com>
2025-09-03 20:55:31 -0300

    modificando a saida do csv
    adicionando uma pasta csv para salvar os dados do csv

--- utils/save.py
+++ utils/save.py
@@ -2,6 +2,7 @@ import pandas as pd
 import csv
 from datetime import datetime
 from logs import log
+import os
 
 def to_csv(data_base: list, vaga: str, cidade: str):
     # Salvando em CSV
@@ -12,13 +13,15 @@ def to_csv(data_base: list, vaga: str, cidade: str):
         data = pd.DataFrame(data_base)
 
         # Salvando CSV
+        name_csv = f"vaga-{(f"{vaga.split()[0]}-{vaga.split()[1]}") if len(vaga.split()) > 1 else vaga}-{cidade}-{datetime.today().date()}.csv"
+        dir_path = os.path.join('csv')
+        file_path = os.path.join(dir_path, name_csv)
+
         data.to_csv(
-            f"vaga-{(f"{vaga.split()[0]}-{vaga.split()[1]}") if len(
-                vaga.split()) > 1 else vaga}-{cidade}-{datetime.today().date()}.csv",
+            file_path,
             index=False,
             encoding='utf-8-sig',
             sep=';',
             lineterminator='\n',
             quoting=csv.QUOTE_MINIMAL
         )
-    
